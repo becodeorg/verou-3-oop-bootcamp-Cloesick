b09ba4865f2e65867094ea3f08d275fe661e074a
Bert Heyman <bert.heyman@hotmail.com>
2022-03-14 15:20:03 +0100

    Refactor products array

--- case-1/arrays/index.php
+++ case-1/arrays/index.php
@@ -6,15 +6,31 @@ function pre($input)
     var_dump($input);
     echo "</pre>";
 }
+
 //A basket contains the following things:
 // A basket contains the following things:
 // - Banana's (6 pieces, costing €1 each)
 // - Apples (3 pieces, costing €1.5 each)
 // - Bottles of wine (2 bottles, costing €10 each)
 $products = [
-    ['name' => 'bananas', 'volume' => '6', 'price' => 1 + "€", 'alcohol' => false], 
-    ['name' => 'apples', 'volume' => '3', 'price' => 1.5 + "€", 'alcohol' => false],
-    ['name' => 'wine bottles', 'volume' => '2', 'price' => 10 + "€", 'alcohol' => true]
+    [
+        'name' => 'bananas',
+        'volume' => 6,
+        'price' => 1,
+        'alcohol' => false
+    ],
+    [
+        'name' => 'apples',
+        'volume' => 3,
+        'price' => 1.5,
+        'alcohol' => false
+    ],
+    [
+        'name' => 'wine bottles',
+        'volume' => 2,
+        'price' => 10,
+        'alcohol' => true
+    ]
 ];
 // TODO: Calculate the total price
 
